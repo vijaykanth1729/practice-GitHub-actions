637d7df9e8146e33884c4886ab21211008f4a963
vijaykanth1729 <personalvijay007@gmail.com>
2026-04-14 15:33:24 +0530

    added github action CICD flow

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-print("Hello world from our application code..")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello, Flask is accessible by any IP!"
+
+@app.route('/api')
+def hello_api():
+    return "Hello, Flask is accessible by api endpoint..!"
+
+if __name__ == '__main__':
+    # host='0.0.0.0' allows access from any IP address
+    # debug=True enables the interactive debugger and auto-reloader
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
